8a5774398f4ccd14a66c3a01ac6d702f914b45f2
Nikita Kajalin <89911732+kajalinn@users.noreply.github.com>
2022-02-28 15:58:28 +0200

    Update Main.java
    Programm, mis võimaldab leida figuuri ruudu(kolmnurk, ristkülik või ring)

--- 03_klassikomplekt/Main.java
+++ 03_klassikomplekt/Main.java
@@ -1,13 +1,25 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        Rectangular rectangular = new Rectangular();
+        Circle circle = new Circle();
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter figure (triangle, rectangular, circle) : ");
         String name = myObj.nextLine();
-        if (name == "triangle") {
-            Triangle triangle = new Triangle();
+        if (Objects.equals(name, "triangle")) {
+            triangle.choose_sides();
             triangle.findSquare(triangle.x, triangle.y, triangle.z);
+        }
+        if (Objects.equals(name, "rectangular")) {
+            rectangular.choose_sides();
+            rectangular.findSquare(rectangular.a, rectangular.b);
+        }
+        if (Objects.equals(name, "circle")) {
+            circle.choose_radius();
+            circle.findSquare(circle.radius);
         }
 
     }
