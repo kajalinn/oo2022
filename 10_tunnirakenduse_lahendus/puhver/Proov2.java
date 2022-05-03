3404fd77fa59bec88a7b692b6d75a40b012263d7
Nikita Kajalin <89911732+kajalinn@users.noreply.github.com>
2022-05-03 14:29:53 +0300

    Add files via upload

---
+++ 10_tunnirakenduse_lahendus/puhver/Proov2.java
@@ -0,0 +1,8 @@
+public class Proov2{
+    public static void main(String[] args) {
+        Arvutaja<String, String> a = new Puhverdaja<String, String>(new EsitaheLeidja());
+        System.out.println(a.f("Tere"));
+        System.out.println(a.f("Kere"));
+        System.out.println(((Puhverdaja)a).puhver);
+    }
+}
